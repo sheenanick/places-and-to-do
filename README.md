a8fbe8359346692d280024b80a5defbffe0251a5
Sheena Nickerson and Yusuf Qedan <sheena@>
2016-08-22 16:16:17 -0700

    add form to add to-do list items and all behaviors that are associated with the list, also fixed a bug in the places code that caused pre made items not re appearing after 2 new places were added

---
+++ README.md
@@ -0,0 +1,36 @@
+# _Places  and Todo list_
+
+#### _A web page which keeps track of your most recently visited place and todo list application. August, 22nd, 2016_
+
+#### By _**Sheena Nickerson and Yusuf Qedan**_
+
+## Behaviors
+##### Places
+* _When a user fills in the form the data will be stored in an object INPUT: Portland, VooDoo Doughnuts, Summer, Portland has a lot of bridges, OUTPUT: all values are stored in a object with parameters location, landmark, timeOfYear, notes_
+
+##### To Do List
+* _When a user fills in a task the data is stored in an object INPUT: Clean room, Normal, 08/23/2016, OUTPUT: all values are stored in a object with parameters description, priority, date_
+* _Color of task is determined depending on priority of task INPUT: Low OUTPUT: #005ce6_
+
+## Setup/Installation Requirements for leap year detector
+* _Clone this repository to your desktop_
+* _Open index.html in the browser of your choosing_
+
+## Known Bugs
+_None yet_
+
+## Support and contact details
+* _sheenanick@gmail.com_
+* _yusuf9191@yahoo.com_
+
+## Technologies Used
+_HTML,
+CSS,
+Bootstrap,
+JS,
+jQuery_
+
+### License
+The MIT License (MIT)
+
+Copyright (c) 2016 **_Sheena Nickerson and Yusuf Qedan_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,88 @@
-var Place = function(location, landmark, timeOfYear, notes){
+var Place = function(location, landmark, image, timeOfYear, notes){
   this.loc = location;
   this.landmark = landmark;
+  this.image = image;
   this.timeOfYear = timeOfYear;
   this.notes = notes;
 }
-Place.prototype.displayDetails = function(){
-  $("#properties").hide().append("<li class='place'><p class ='location'>Location: "+ this.loc +"</p><div class='hiddenPlace'><p>Landmark: " +this.landmark + " </p><p>Best time of year to visit: " + this.timeOfYear + "</p><p>Notes: "+ this.notes +"</p> </div></li>").slideDown();
+
+var Task = function(description, priority, date){
+  this.desc = description;
+  this.priority = priority;
+  this.date = date;
 }
-var formSubmitted = false;
+
+Place.prototype.displayDetails = function(){
+  $("#places").hide().append("<li class='place'><p class='loc'>"+ this.loc +"</p><div class='hiddenPlace'><ul><li>Landmark: " +this.landmark + " <br><img src='" + this.image + "' style='width:200px'></li><br><li>Best time of year to visit: " + this.timeOfYear + "</li><br><li>Notes: "+ this.notes +"</li></ul></div></li>").slideDown();
+}
+Task.prototype.displayTask = function(){
+  var color = "";
+  if(this.priority == "High")
+    color = "#FF0000"
+  else if (this.priority == "Normal") {
+    color = "#000000"
+  }
+  else{//low
+    color = "#005ce6"
+  }
+
+  $("#tasks").append("<li><span class='task' style=color:"+ color+ ">"+this.desc+ "</span><span class='done'><br>Done?</span></li>");
+}
+Task.prototype.showBox = function(){
+  $("#taskBoxDesc").text( this.desc +":");
+  $("#taskBoxPriority").text("Priority Level: "+ this.priority);
+  if(this.priority =="High"){
+    $("#taskBoxPriority").css("color","#FF0000");
+  }
+  else if (this.priority =="Normal") {
+      $("#taskBoxPriority").css("color","#000000");
+  }
+  else{ //Low
+      $("#taskBoxPriority").css("color","#005ce6");
+  }
+  $("#taskBoxDate").text("Date to complete by: "+ this.date );
+}
 
 $(document).ready(function() {
   $("form#addLocation").submit(function(event){
     event.preventDefault();
-    formSubmitted = true;
     var location = $("#location").val();
     var landmark = $("#landmark").val();
+    var image = $("#image").val();
     var timeOfYear = $("#timeOfYear").val();
     var notes = $("#notes").val();
-    var myPlace = new Place(location, landmark, timeOfYear, notes);
+    var myPlace = new Place(location, landmark, image, timeOfYear, notes);
     myPlace.displayDetails();
-    $(".place").click(function(){
-      $("#properties").find("div").slideUp();
+    $(".place").last().click(function(){
+      $("#places").find("div").slideUp();
       if (!($(this).find("div").is(':visible'))){
         $(this).find("div").slideDown();
       }
     });
   });
   $(".place").click(function(){
-    if(!(formSubmitted)){
-      $("#properties").find("div").slideUp();
+      $("#places").find("div").slideUp();
       if (!($(this).find("div").is(':visible'))){
         $(this).find("div").slideDown();
       }
-    }
+  });
+  $("form#toDo").submit(function(event){
+    event.preventDefault();
+    var description = $("#taskDescription").val();
+    var priority = $("#priority").val();
+    var date = $("#date").val();
+    var myTask = new Task(description, priority, date);
+    myTask.displayTask();
+    $(".task").last().click(function(){
+      $(".taskBox").show();
+      myTask.showBox();
+    });
+    $(".done").last().click(function(){
+      var done = confirm("Are you sure " + myTask.desc + " is complete?");
+      if(done){
+        $(this).parent().remove();
+        $(".taskBox").hide();
+      }
+    });
   });
 });
